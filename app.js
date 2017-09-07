02d3496133ed24b41245181dafc0920649181ec6
Wilson Li <b84330808@gmail.com>
2017-09-07 15:50:33 +0800

    Update

--- app.js
+++ app.js
@@ -2,44 +2,59 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(3001);
-
 var SerialPort = require('serialport');
 
-// var port = new SerialPort('COM12', {
-//     baudRate: 9600
-// });
+var serial = new SerialPort('/dev/cu.usbmodem1421', {
+    baudRate: 9600
+});
 
 
-// port.write('main screen turn on', function(err) {
-//     if (err) {
-//         return console.log('Error on write: ', err.message);
-//     }
-//     console.log('message written');
-// });
+// Open errors will be emitted as an error event
+serial.on('error', function(err) {
+    console.log('Error: ', err.message);
+})
+serial.on('open', function(){
+    console.log('Serial Port Opend');
+    serial.on('data', function(data){
+        console.log(data);
+    });
+});
 
-// // Open errors will be emitted as an error event
-// port.on('error', function(err) {
-//     console.log('Error: ', err.message);
-// })
+
 
 var port = parseInt(process.env.PORT, 10) || 3000;
 
 app.use(express.static(__dirname + '/'));
-app.set('port', port);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/a.html')
 })
 
 app.listen(port, function() {
     console.log('Example app listening on port 3000!')
 })
 
+
+
+/**
+ * Socket.io
+ */
 io.on('connection', function(client) {
     console.log('Client connected...');
     // client.emit('news', { hello: 'world' });
     client.on('send', function(data) {
+
         //send to serialport
-        console.log(data);
+        serial.write(data+"\n", function(err) {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('message written:'+data);
+        });
     })
 });
